2029df848f047faa37625d5299e2215a3b7bb8ca
Karl Ljungkvist <k.ljungkvist@gmail.com>
2017-02-13 17:54:39 +0100

    add metric coefficient for general geometry

--- include/stenseal/metric_coefficient.h
+++ include/stenseal/metric_coefficient.h
@@ -50,6 +50,71 @@ namespace stenseal
     }
   };
 
+  template <int dim>
+  struct MetricCoefficient<dim,GeneralGeometry<dim>>
+  {
+  private:
+    std::vector<double> c;
+    unsigned int n_points;
+
+  public:
+    template <typename DmOp>
+    MetricCoefficient(const GeneralGeometry<dim> &g, const DmOp &op);
+
+    double get(int i) const
+    {
+      return c[i];
+    }
+
+    template <int n>
+      std::array<double, n> get_centered_array(int i) const
+    {
+      std::array<double, n> res;
+      const std::array<int, n> &offsets = internal::gen_offsets<n>::value;
+      for(int j = 0; j < n; ++j)
+        res[j] = c[i+offsets[j]];
+      return res;
+    }
+
+    template <int n>
+      std::array<double, n> get_left_boundary_array() const
+    {
+      std::array<double, n> res;
+      for(int j = 0; j < n; ++j)
+        res[j] = c[j];
+      return res;
+    }
+
+    template <int n>
+      std::array<double, n> get_right_boundary_array() const
+    {
+      std::array<double, n> res;
+      for(int j = 0; j < n; ++j)
+        res[j] = c[n_points-n+j];
+      return res;
+    }
+  };
+
+
+  template <int dim>
+  template <typename DmOp>
+  MetricCoefficient<dim,GeneralGeometry<dim>>::MetricCoefficient(const GeneralGeometry<dim> &g, const DmOp &op)
+  {
+    if(dim==1) {
+      const std::vector<dealii::Point<dim>> &pts = g.get_points();
+      const unsigned int npts = pts.size();
+      std::vector<double> xvals(npts);
+
+      for(int i=0; i<npts; ++i)
+        xvals[i] = pts[i](0);
+
+      c.resize(npts);
+      op.apply(c,xvals);
+    }
+    else {
+      AssertThrow(false,dealii::ExcNotImplemented());
+    }
+  }
 
 
   namespace internal
